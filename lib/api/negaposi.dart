b18853c0d20f604289334d47cf2f42c07daa12ad
TatsuyaTaira <longye.jap@gmail.com>
2021-10-24 23:11:17 +0900

    [wip] negaposi.dart 36行目エラーの作業途中

--- lib/api/negaposi.dart
+++ lib/api/negaposi.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class NegaposiScreen extends StatelessWidget {
   @override
@@ -18,10 +21,32 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
+class SampleResponse{
+  final String word_list;
+  SampleResponse({
+    required this.word_list,
+  });
+  factory SampleResponse.fromJson(Map<String, dynamic> json) {
+    return SampleResponse(
+      word_list: json["word_list"],
+    );
+  }
+}
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  String resultjson = "";
+  Future<SampleResponse> requestApi() async{
+    var url = Uri.parse("https://labs.goo.ne.jp/api/morph");
+    var request = new SampleRequest(app_id: "7b595a6f365dd0c113ec8f7599694e7f4eda87e2de7868683492ab11f26a9b98",
+        sentence:"ツナマヨとシャケはうまい",info_filter: "form",pos_filter: "名詞");
+    final response = await http.post(url,body: json.encode(request.toJson()), headers: {"Content-Type": "application/json"});
+    if (response.statusCode == 200){
+      var ResultText = SampleResponse.fromJson(json.decode(response.body));
+      resultjson =ResultText.word_list;
+    }else{
+      throw Exception('Failed to load post');
+    }
+  }
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -55,4 +80,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
+class SampleRequest {
+  final String app_id;
+  final String sentence;
+  final String info_filter;
+  final String pos_filter;
+  late final String name;
+  SampleRequest({
+    required this.app_id,
+    required this.sentence,
+    required this.info_filter,
+    required this.pos_filter,
+  });
+  Map<String,dynamic> toJson() =>{
+    "app_id":app_id,
+    "text":sentence,
+    "info_filter":info_filter,
+    "pos_filter":pos_filter,
+  };
 }
