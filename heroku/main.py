bbbbb5e02a20f81bacc28c8870549781a8d19ce4
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-10-24 15:50:03 +0900

    [add] テストコミット

---
+++ heroku/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Test(BaseModel):
+    Text1: str
+    Text2: str
+
+
+@app.get("/")
+def read_root():
+    return {"Welcome": "Welcome"}
+
+
+@app.post("/test/")
+def test(req: Test):
+    new_str = req.Text1 + req.Text2
+    return {"res": "ok", "text": new_str}
