b84a73be8868ec027c71bfc453c3b392fa5a211e
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-10-25 22:23:22 +0900

    [change] API通信用にスクリーン切り替え

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:a_2102/api/negaposi.dart';
+import 'package:a_2102/api/test_communication.dart';
 import 'package:a_2102/scraping/scraping.dart';
 import 'package:a_2102/ui/title.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(TitleScreen());
+  runApp(TestCommunicationScreen());
 }
