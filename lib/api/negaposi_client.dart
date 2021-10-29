fa9a4fc941f9028ee447d892a6ef1f595ab69c5a
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-10-29 15:05:58 +0900

    [update] 追加のレスポンスを追加

--- lib/api/negaposi_client.dart
+++ lib/api/negaposi_client.dart
@@ -23,15 +23,39 @@ Future<NegaposiRes> createNegaposiRes(String text1, String text2) async {
 
 class NegaposiRes {
   final String res;
+  final String imageUrl_1;
+  final String imageUrl_2;
   final double score_1;
   final double score_2;
+  final List<dynamic> sentence_1;
+  final List<dynamic> sentence_2;
+  final List<dynamic> siteUrl_1;
+  final List<dynamic> siteUrl_2;
 
-  NegaposiRes(
-      {required this.res, required this.score_1, required this.score_2});
+  NegaposiRes({
+      required this.res,
+      required this.imageUrl_1,
+      required this.imageUrl_2,
+      required this.score_1,
+      required this.score_2,
+      required this.sentence_1,
+      required this.sentence_2,
+      required this.siteUrl_1,
+      required this.siteUrl_2,
+      });
 
   factory NegaposiRes.fromJson(Map<String, dynamic> json) {
     return NegaposiRes(
-        res: json['res'], score_1: json['score_1'], score_2: json['score_2']);
+        res: json['res'],
+        imageUrl_1: json['image_url_1'],
+        imageUrl_2: json['image_url_2'],
+        score_1: json['score_1'],
+        score_2: json['score_2'],
+        sentence_1: json['sentence_1'],
+        sentence_2: json['sentence_2'],
+        siteUrl_1: json['site_url_1'],
+        siteUrl_2: json['site_url_2'],
+        );
   }
 }
 
@@ -111,8 +135,14 @@ class _NegaposiClientAppState extends State<NegaposiClientApp> {
           return Column(
             children: [
               Text(snapshot.data!.res),
+              Text(snapshot.data!.imageUrl_1),
+              Text(snapshot.data!.imageUrl_2),
               Text(snapshot.data!.score_1.toString()),
               Text(snapshot.data!.score_2.toString()),
+              Text(utf8.decode(snapshot.data!.sentence_1.toString().runes.toList())),
+              Text(utf8.decode(snapshot.data!.sentence_2.toString().runes.toList())),
+              Text(snapshot.data!.siteUrl_1.toString()),
+              Text(snapshot.data!.siteUrl_2.toString()),
             ],
           );
         } else if (snapshot.hasError) {
