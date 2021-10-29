b7cc256ce755619a7f43de126c959b8303fa68df
momomomo111 <momomomo111@users.noreply.github.com>
2021-10-29 08:12:24 +0000

    deploy: dd440c066fd848ce058e7d7e351096daad554508

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "main.dart.js": "e75412ceee83ad4a5e2f9c36402407a4",
 "manifest.json": "0bf4a91a158f58a807fa8c9f32dde095",
-"index.html": "8dff6375365151acd8e18efd975c8411",
-"/": "8dff6375365151acd8e18efd975c8411"
+"index.html": "560baeb04e21379c0d623ca291a5036e",
+"/": "560baeb04e21379c0d623ca291a5036e"
 };
 
 // The application shell files that are downloaded before a service worker can
