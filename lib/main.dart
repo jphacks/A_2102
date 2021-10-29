1843f175d057a1527153fa257b17cfa89fe55c3b
TatsuyaTaira <longye.jap@gmail.com>
2021-10-30 04:15:38 +0900

    [add] タイムライン画面の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:a_2102/api/negaposi_client.dart';
 import 'package:a_2102/api/test_communication.dart';
 import 'package:a_2102/pages/view/comparison_result.dart';
-import 'package:a_2102/pages/view/scraping_screen.dart';
 import 'package:a_2102/pages/view/sentences_screen.dart';
 import 'package:a_2102/pages/view/title.dart';
+import 'package:a_2102/ui/time_line.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     GetMaterialApp(
       //unknownRoute: GetPage(name: '/notfound', page: () => UnknownRoutePage()),
@@ -16,6 +19,7 @@ void main() {
         GetPage(name: '/', page: () => TitleScreen()),
         GetPage(name: '/result', page: () => ComparisonResultScreen()),
         GetPage(name: '/sentences', page: () => SentencesScreen()),
+        GetPage(name: '/timeline', page: () => TimeLine()),
       ],
     ),
   );
