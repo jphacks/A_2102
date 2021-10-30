a33267d99bffd10513eb349cfea592cba44c250c
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-10-30 13:33:13 +0900

    [release] web用にFirebaseの機能削減

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:a_2102/pages/view/comparison_result.dart';
 import 'package:a_2102/pages/view/sentences_screen.dart';
 import 'package:a_2102/pages/view/title.dart';
-import 'package:a_2102/ui/time_line.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:a_2102/ui/time_line.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(
     GetMaterialApp(
       //unknownRoute: GetPage(name: '/notfound', page: () => UnknownRoutePage()),
@@ -17,7 +17,7 @@ void main() async{
         GetPage(name: '/', page: () => TitleScreen()),
         GetPage(name: '/result', page: () => ComparisonResultScreen()),
         GetPage(name: '/sentences', page: () => SentencesScreen()),
-        GetPage(name: '/timeline', page: () => TimeLine()),
+        // GetPage(name: '/timeline', page: () => TimeLine()),
       ],
     ),
   );
--- lib/ui/time_line.dart
+++ lib/ui/time_line.dart
@@ -1,47 +1,47 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get.dart';
-
-class TimeLine extends StatelessWidget {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
-      .collection('data').snapshots();
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('タイムライン',style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.yellow,
-      ),
-      body: Center(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: _usersStream,
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loadingなう→これだと進行形の二乗");
-            }
-
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                document.data() as Map<String, dynamic>;
-                return ListTile(
-                  title: Text(data['item_1'] + " VS " +data['item_2']),
-                  subtitle: Text((double.parse(data['score_1']) > double.parse(data['score_2']) ? data['item_1']:data['item_2'])+
-                      "の勝ち"),
-                );
-              }).toList(),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.toNamed('/timeline'),
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get_core/src/get_main.dart';
+// import 'package:get/get.dart';
+//
+// class TimeLine extends StatelessWidget {
+//   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
+//       .collection('data').snapshots();
+//
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('タイムライン',style: TextStyle(color: Colors.black)),
+//         backgroundColor: Colors.yellow,
+//       ),
+//       body: Center(
+//         child: StreamBuilder<QuerySnapshot>(
+//           stream: _usersStream,
+//           builder:
+//               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return Text("Loadingなう→これだと進行形の二乗");
+//             }
+//
+//             return ListView(
+//               children: snapshot.data!.docs.map((DocumentSnapshot document) {
+//                 Map<String, dynamic> data =
+//                 document.data() as Map<String, dynamic>;
+//                 return ListTile(
+//                   title: Text(data['item_1'] + " VS " +data['item_2']),
+//                   subtitle: Text((double.parse(data['score_1']) > double.parse(data['score_2']) ? data['item_1']:data['item_2'])+
+//                       "の勝ち"),
+//                 );
+//               }).toList(),
+//             );
+//           },
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => Get.toNamed('/timeline'),
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
