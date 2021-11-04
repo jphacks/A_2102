e3be48a6ffe2dcf200534b1714be307686979fc9
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2021-11-04 21:08:41 +0900

    [update] ライブラリをアップデート

--- heroku/main.py
+++ heroku/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from scraping import get_thumbnail, scrape
 from inference import NegaPogi
 from pydantic import BaseModel
